34caf2a28708e976aba4a3d19d134e8fb0ae598b
Shuzheng Zheng <shuzheng9811@gmail.com>
2019-09-15 13:40:25 -0400

    JS/advdatainput.js: add function to get today's date
    return string in the format of mm/dd/yyyy
    
    Signed-off-by: Shuzheng Zheng <shuzheng9811@gmail.com>

--- JS/advanceddatainput.js
+++ JS/advanceddatainput.js
@@ -57,6 +57,16 @@ function setup() {
     }
 }
 
+function getDateToday() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+
 //This block is for selecting the identifiers that are saved with each piece of data
 
 function choosePlayer(choice) {
